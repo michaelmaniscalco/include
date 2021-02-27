ceaaf01e03f334aea9007a9a5b92915712642801
michael <michael@michael-maniscalco.com>
2021-02-27 16:18:02 -0500

    first commit

---
+++ src/include/endian/endian_type.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace maniscalco
+{
+
+    struct big_endian_type;
+    struct little_endian_type;
+
+    using network_order_type = big_endian_type;
+    using host_order_type = little_endian_type;
+   //    using host_order_type = big_endian_type;
+}
