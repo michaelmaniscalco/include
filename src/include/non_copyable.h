90d432af70d2f974680fbe99189672e03db55857
michael <michael@michael-maniscalco.com>
2023-01-07 03:26:30 -0500

    non_copyable and non_movable

--- src/include/non_copyable.h
+++ src/include/non_copyable.h
@@ -6,6 +6,8 @@ namespace maniscalco
 
     struct non_copyable
     {
+        non_copyable() noexcept = default;
+        ~non_copyable() noexcept = default;
         non_copyable(non_copyable const &) = delete;
         non_copyable & operator = (non_copyable const &) = delete;
     }; // struct non_copyable
--- src/include/non_movable.h
+++ src/include/non_movable.h
@@ -6,6 +6,8 @@ namespace maniscalco
 
     struct non_movable
     {
+        non_movable() noexcept = default;
+        ~non_movable() noexcept = default;
         non_movable(non_movable &&) = delete;
         non_movable & operator = (non_movable &&) = delete;
     }; // struct non_movable
