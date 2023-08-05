8e6127c27f5b7fa9d7d782b2615bdcd5c767de91
michael <michael@michael-maniscalco.com>
2023-08-05 11:21:19 -0400

    clean up endian

--- src/include/endian/endian.h
+++ src/include/endian/endian.h
@@ -2,14 +2,14 @@
 
 #include <bit>
 
-
+/*
 namespace maniscalco
 {
     
     template <typename, std::endian> class endian;
 
 } // namespace maniscalco
-
+*/
 
 #include "./byte_swap.h"
 #include "./endian_swap.h"
@@ -23,6 +23,15 @@ namespace maniscalco
 namespace maniscalco
 {
 
+    template <typename data_type, std::endian endian_type>
+    class endian;
+
+
+    //=========================================================================
+    template <typename T>
+    concept endian_concept = std::is_same_v<T, endian<typename T::underlying_type, T::type>>;
+
+
     //==============================================================================
     template <typename data_type, std::endian endian_type>
     class endian
@@ -34,7 +43,7 @@ namespace maniscalco
 
         template <typename, std::endian> friend class endian;
 
-        endian();
+        endian() = default;
 
         endian
         (
@@ -66,14 +75,78 @@ namespace maniscalco
             underlying_type const &
         );
 
-        operator underlying_type() const;
+        template <endian_concept T_>
+        requires (std::is_convertible_v<underlying_type, typename T_::underlying_type>)
+        operator T_
+        (
+        ) const
+        {
+            typename T_::underlying_type destination(this->get());
+            return T_(destination);
+        }
 
         underlying_type get() const;
+
+        auto operator <=> 
+        (
+            endian const & other
+        ) const
+        {
+            return (get() <=> other.get());
+        }
+
+        auto operator <=> 
+        (
+            underlying_type other
+        ) const
+        {
+            return (get() <=> other);
+        }
+
+        template <typename T>
+        requires (std::integral<T> || std::floating_point<T>) &&
+        requires (T t, underlying_type v){v + t;}
+        auto operator +(T value) const{return (get() + value);}
+
+        template <typename T>
+        requires (std::integral<T> || std::floating_point<T>) &&
+        requires (T t, underlying_type v){v += t;}
+        endian & operator += (T value){*this = endian(get() + value); return *this;}
+
+        template <typename T>
+        requires (std::integral<T> || std::floating_point<T>) &&
+        requires (T t, underlying_type v){v - t;}
+        auto operator -(T value) const{return (get() - value);}
+
+        template <typename T>
+        requires (std::integral<T> || std::floating_point<T>) &&
+        requires (T t, underlying_type v){v -= t;}
+        endian & operator -= (T value){*this = endian(get() - value); return *this;}
+
+        template <typename T>
+        requires (std::integral<T> || std::floating_point<T>) &&
+        requires (T t, underlying_type v){v * t;}
+        auto operator *(T value) const{return (get() * value);}
+
+        template <typename T>
+        requires (std::integral<T> || std::floating_point<T>) &&
+        requires (T t, underlying_type v){v *= t;}
+        endian & operator *= (T value){*this = endian(get() * value); return *this;}
+
+        template <typename T>
+        requires (std::integral<T> || std::floating_point<T>) &&
+        requires (T t, underlying_type v){v / t;}
+        auto operator /(T value) const{return (get() / value);}
+
+        template <typename T>
+        requires (std::integral<T> || std::floating_point<T>) &&
+        requires (T t, underlying_type v){v /= t;}
+        endian & operator /= (T value){*this = endian(get() / value); return *this;}
 
     private:
 
         underlying_type  value_;
-    }; // class endian
+    };
 
 
     template <typename data_type> using big_endian = endian<data_type, std::endian::big>;
@@ -81,30 +154,7 @@ namespace maniscalco
     template <typename data_type> using network_order = endian<data_type, std::endian::big>;
     template <typename data_type> using host_order = endian<data_type, std::endian::native>;
 
-    template <typename T>
-    concept endian_concept = std::is_same_v<T, endian<typename T::underlying_type, T::type>>;
-
-
-    template <endian_concept T>
-    static std::string to_string
-    (
-        T const & source
-    )
-    {
-        return to_string(source.get());
-    }
-
 } // namespace maniscalco
-
-
-//==============================================================================
-template <typename data_type, std::endian endian_type>
-maniscalco::endian<data_type, endian_type>::endian
-(
-):
-    value_()
-{
-}
 
 
 //==============================================================================
@@ -134,9 +184,9 @@ template <typename data_type, std::endian endian_type>
 maniscalco::endian<data_type, endian_type>::endian
 (
     data_type const & input
-):
-    value_(endian_swap<std::endian::native, endian_type>(input))
+)
 {
+    value_ = endian_swap<std::endian::native, endian_type>(input);
 }
 
 
@@ -178,68 +228,9 @@ auto maniscalco::endian<data_type, endian_type>::operator =
 
 //==============================================================================
 template <typename data_type, std::endian endian_type>
-maniscalco::endian<data_type, endian_type>::operator underlying_type
-(
-) const
-{
-    return endian_swap<endian_type, std::endian::native>(value_);
-}
-
-
-//==============================================================================
-template <typename data_type, std::endian endian_type>
 auto maniscalco::endian<data_type, endian_type>::get
 (
 ) const -> underlying_type
 {
     return endian_swap<endian_type, std::endian::native>(value_);
-}
-
-
-//=============================================================================
-template <maniscalco::endian_concept T>
-static std::ostream & operator <<
-(
-    std::ostream & stream,
-    T const & source
-)
-{
-    return stream << source.get();
-}
-
-
-//=============================================================================
-static auto operator <=> 
-(
-    maniscalco::endian_concept auto const & first,
-    maniscalco::endian_concept auto const & second
-)
-{
-    return (first.get() <=> second.get());
-}
-
-
-//=============================================================================
-template <typename T>
-requires (!maniscalco::endian_concept<T>)
-static auto operator <=> 
-(
-    maniscalco::endian_concept auto const & first,
-    T const & second
-)
-{
-    return (first.get() <=> second);
-}
-
-
-//=============================================================================
-template <typename T>
-requires (!maniscalco::endian_concept<T>)
-static auto operator <=> 
-(
-    T const & first,
-    maniscalco::endian_concept auto const & second
-)
-{
-    return (first <=> second.get());
 }
